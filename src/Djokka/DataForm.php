bd839700a3e578ee92e19f0155cf3b8536f1ba22
ahmjw <rawndummy@gmail.com>
2014-02-26 23:48:17 +0800

    Declare class body

--- src/Djokka/DataForm.php
+++ src/Djokka/DataForm.php
@@ -2,25 +2,6 @@
 
 namespace Djokka;
 
-class DataForm
+class DataForm extends Model
 {
-	/**
-     * Mengambil instance suatu model dari peta model
-     * @param mixed $class Nama kelas model
-     * @param mixed $module Nama modul tempat meletakkan model tersebut
-     * @param optional $is_new apakah model akan dimuat sebagai data baru atau data lama
-     * @return object
-     */
-    private static function getObject($class, $module)
-    {
-        $schema = SchemaCollection::get();
-        if(!$schema->existsModel($class)) {
-            $object = new $class;
-            $object->____dataset['module'] = $module;
-            $schema->models($module, $object);
-        } else {
-            $object = $schema->models($module);
-        }
-        return $object;
-    }
 }
